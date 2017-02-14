5b9348459f7d0dfdb4f2bded473eedb36beef052
dapwn <k0d0c0z0@gmail.com>
2017-02-14 14:08:14 +0100

    Fixed UK size checking

--- Visual Studio/3s_atc/Helpers.cs
+++ Visual Studio/3s_atc/Helpers.cs
@@ -319,6 +319,11 @@ namespace _3s_atc
             eu_sizes[4] = "36"; eu_sizes[4.5] = "36 2/3"; eu_sizes[5] = "37 1/3"; eu_sizes[5.5] = "38"; eu_sizes[6] = "38 2/3"; eu_sizes[6.5] = "39 1/3"; eu_sizes[7] = "40"; eu_sizes[7.5] = "40 2/3"; eu_sizes[8] = "41 1/3"; eu_sizes[8.5] = "42"; eu_sizes[9] = "42 2/3"; eu_sizes[9.5] = "43 1/3"; eu_sizes[10] = "44"; eu_sizes[10.5] = "44 2/3"; eu_sizes[11] = "45 1/3"; eu_sizes[11.5] = "46"; eu_sizes[12] = "46 2/3"; eu_sizes[12.5] = "47 1/3"; eu_sizes[13] = "48"; eu_sizes[13.5] = "48 2/3"; eu_sizes[14] = "49 1/3"; eu_sizes[14.5] = "50";
             return eu_sizes[size];
         }
+
+        private string getUKSize(double size)
+        {
+            return (size - 0.5).ToString("0.#").Replace(',', '.');
+        }
         private string getFirstAvailableSize(List<double> sizes, string pid)
         {
             Dictionary<string, Dictionary<string, string>> inventory = getInventory(pid);
@@ -329,9 +334,17 @@ namespace _3s_atc
                 {
                     string s = inventory[entry.Key]["size"];
                     int stock = Convert.ToInt32(inventory[entry.Key]["stockcount"]);
-                    
-                    if (getEUSize(size).Replace(" ", String.Empty) == s.Replace(" ", String.Empty) && stock > 0  || s == size.ToString("0.#").Replace(',', '.') && stock > 0)
+
+                    if (Properties.Settings.Default.code == "GB")
+                    {
+                        if (s == getUKSize(size) && stock > 0)
                             return entry.Key;
+                    }
+                    else
+                    {
+                        if (getEUSize(size).Replace(" ", String.Empty) == s.Replace(" ", String.Empty) && stock > 0 || s == size.ToString("0.#").Replace(',', '.') && stock > 0)
+                            return entry.Key;
+                    }
                 }
             }
 
